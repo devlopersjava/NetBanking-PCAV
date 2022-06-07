795131cf8b5c5e944b88ebae0d2e7e9677752e9a
Admin <Admin@DESKTOP-QDK5I68>
2022-06-07 18:42:45 +0530

    login change

--- NetBankingProject/src/main/java/com/netbanking/main/controller/LoginController.java
+++ NetBankingProject/src/main/java/com/netbanking/main/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
 //	public boolean addNewEmployee(Login login ) {
 //		return loginServiceInterface.addLogin(login);
 //	}
-//
+
 //	@RequestMapping(value="login",method = RequestMethod.PUT)
 //	public boolean updateLoginByUserId(Login login) {
 //		return loginServiceInterface.updateLoginByUserId(login);
